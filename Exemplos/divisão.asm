159af6ddcb7c72b1fc71715b91c8c08f6de740ba
Flavio Roberto O. dos Santos <53623916+FlaviodosSantos@users.noreply.github.com>
2019-10-04 14:39:22 -0300

    Update divisão.asm

--- Exemplos/divisão.asm
+++ Exemplos/divisão.asm
@@ -1,16 +1,16 @@
-# exemplo de divis„o
+# exemplo de divis√£o
 .data
-	mensagem: .asciiz "\nO resultado da divis„o È : "
+	mensagem: .asciiz "\nO resultado da divis√£o √© : "
 
 .text 
 	li $s2, 10		# guarda o vr 10 em s2
 	li $s3, 2		# guarda o vr 2 em s3
 	div $s1, $s2, $s3	# divide s2 por s3 e guarda em s1
 
-	li $v0, 4		# 4 È o comando para imprimir texto
-	la $a0, mensagem 	# garada a mensagem em a0
+	li $v0, 4		# 4 √© o comando para imprimir texto
+	la $a0, mensagem 	# guarda a mensagem em a0
 	syscall 		# chamada do sistema e executa o print
 
- 	li $v0, 1		# 1 È o comando para imprimir inteiro
- 	la $a0, ($s1)		# guarada o inteiro em a0
+ 	li $v0, 1		# 1 √© o comando para imprimir inteiro
+ 	la $a0, ($s1)		# guarda o inteiro em a0
  	syscall			# chamada do sistema e executa o print
